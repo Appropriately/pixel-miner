e25e947f8d7665d9a6d35a66591f640cf7041749
Sean Lewis <sean.alan.lewis@gmail.com>
2017-10-28 00:27:05 +0100

    Added check to see if enough to buy

--- js/controller.js
+++ js/controller.js
@@ -2,11 +2,18 @@ var app = angular.module('ClickerGame', []);
 
   app.controller('BuildingManager', function ($scope, $http, $interval) {
     $scope.pixels = 0;
+    $scope.totalPixels = 0;
+    
+    $scope.increment = function () {
+      $scope.pixels++; $scope.totalPixels++;
+    }
 
     $scope.buy = function (building) {
-      $scope.pixels -= building.price;
+      $scope.pixels -= price(building);
       building.total++;
     }
+
+    $scope.calculate = function(building){return price(building);}
 
     $http.get("data/buildings.txt").then(function (response) {
         $scope.buildings = response.data;
@@ -17,20 +24,26 @@ var app = angular.module('ClickerGame', []);
       // Loop through array of bought buildings
       angular.forEach($scope.buildings, function(building) {
         $scope.pixels += building.total * building.increment;
+        $scope.totalPixels += building.total * building.increment;
       });
-
-
     },1000);
   });
 
+// Filter for checking if the building should be displayed depending on the
+// current number of pixels
 app.filter('canAfford', function () {
   return function (input,scope) {
     var out = [];
     angular.forEach(input, function (building) {
-      if (building.price / 2 <= scope.pixels) {
+      if (building.price / 2 <= scope.totalPixels) {
         out.push(building)
       }
     });
     return out;
   }
 });
+
+// Calculates the price
+function price(building) {
+  return building.price * Math.pow(2, building.total);
+} // price
