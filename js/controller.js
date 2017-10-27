fcc0fcc30af4994028f7a3c130412c6e4aa247b2
Sean Lewis <sean.alan.lewis@gmail.com>
2017-10-27 19:56:18 +0100

    Basic functionality added

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,33 @@
-  angular.module('ClickerGame', [])
-    .controller('Clicker', ['$scope', function ($scope) {
-      $scope.pixels = 0;
-    }]);
+var app = angular.module('ClickerGame', []);
+
+  app.controller('BuildingManager', function ($scope, $http, $interval) {
+    $scope.pixels = 0;
+    $scope.currentBuildings = [];
+
+    $scope.buy = function (building) {
+      console.log(building.name);
+      console.log($scope.currentBuildings);
+      $scope.currentBuildings.push(building);
+      $scope.pixels -= building.price;
+    }
+
+    $http.get("data/buildings.txt").then(function (response) {
+        $scope.buildings = response.data;
+      });
+    
+    $interval(function() { angular.forEach($scope.currentBuildings, function(building) {
+      $scope.pixels += building.increment;
+    })},1000);
+  });
+
+app.filter('canAfford', function () {
+  return function (input,scope) {
+    var out = [];
+    angular.forEach(input, function (building) {
+      if (building.price / 2 <= scope.pixels) {
+        out.push(building)
+      }
+    });
+    return out;
+  }
+});
